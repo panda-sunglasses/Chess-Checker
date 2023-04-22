fc1b4434edce35bbe65c8bc46589ddd5c6524a23
panda-sunglasses <131478977+panda-sunglasses@users.noreply.github.com>
2023-04-22 00:11:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-# Output should be 7
+
 pawns = {'e5', 'd4', 'b2', 'a1', 'h8', 'f6', 'g7', 'c3'}
 x = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h'}
 y = [8, 7, 6, 5, 4, 3, 2, 1]
@@ -21,16 +21,15 @@ for i in pawns:
         x_keys += [key for key, value in x.items() if value == i[0]]
         row.append(int(i[1]))
 
-# Create lists of x coordinates one column ahead and one behind those in pawns
+# Create lists of x coordinates one ahead and one behind those in pawns
 x_vals_plus += [x[i + 1] for i in x_keys if i < 8]
 x_vals_minus += [x[i - 1] for i in x_keys if i > 1]
-y_vals_plus = [i for i in row if i < 8]
-y_vals_minus = [i for i in row if i > 1]
+y_vals = [i for i in row if i < 8 or i > 1]
 
 # Concatenate x and y coordinates to be checked in two lists, one for xy + 1 and xy - 1
 for i in range(len(row) - abs(len(x_vals_plus) - len(row))):
-    safe_check_plus.append(''.join(str(x_vals_plus[i]) + '' + str(y_vals_plus[i] - 1)))
-    safe_check_minus.append(''.join(str(x_vals_minus[i]) + '' + str(y_vals_minus[i] - 1)))
+    safe_check_plus.append(''.join(str(x_vals_plus[i]) + '' + str(y_vals[i] - 1)))
+    safe_check_minus.append(''.join(str(x_vals_minus[i]) + '' + str(y_vals[i] - 1)))
 
 # Check if the coordinates in safe_check_plus/minus lists == 1. If so, add 1 to int(safe)
 for i, j in zip(safe_check_plus, safe_check_minus):
